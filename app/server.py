e826040723309daa821f71407b7fa8ef87b9ac4d
서장원_T4105 <jangwon.seo@snu.ac.kr>
2023-01-27 21:27:25 +0900

    :rotating_light: Pass application as an import string WARNING:  You must pass the application as an import string to enable 'reload' or 'workers'.

--- app/server.py
+++ app/server.py
@@ -183,5 +183,8 @@ if __name__ == "__main__":
     import uvicorn
 
     uvicorn.run(
-        app, host=config["server"]["host"], port=config["server"]["port"], reload=True
+        "app.server:app",
+        host=config["server"]["host"],
+        port=config["server"]["port"],
+        reload=True,
     )
