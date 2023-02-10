92c5bcffc690dbfc70d045c5aa737cf8f3af46c5
서장원_T4105 <jangwon.seo@snu.ac.kr>
2023-02-10 10:23:20 +0900

    :sparkle: Set wandb display name and tags from config filename

--- mmyolo/tools/train.py
+++ mmyolo/tools/train.py
@@ -13,41 +13,45 @@ from mmyolo.utils import register_all_modules
 
 
 def parse_args():
-    parser = argparse.ArgumentParser(description='Train a detector')
-    parser.add_argument('config', help='train config file path')
-    parser.add_argument('--work-dir', help='the dir to save logs and models')
+    parser = argparse.ArgumentParser(description="Train a detector")
+    parser.add_argument("config", help="train config file path")
+    parser.add_argument("--work-dir", help="the dir to save logs and models")
     parser.add_argument(
-        '--amp',
-        action='store_true',
+        "--amp",
+        action="store_true",
         default=False,
-        help='enable automatic-mixed-precision training')
+        help="enable automatic-mixed-precision training",
+    )
     parser.add_argument(
-        '--resume',
-        nargs='?',
+        "--resume",
+        nargs="?",
         type=str,
-        const='auto',
-        help='If specify checkpoint path, resume from it, while if not '
-        'specify, try to auto resume from the latest checkpoint '
-        'in the work directory.')
+        const="auto",
+        help="If specify checkpoint path, resume from it, while if not "
+        "specify, try to auto resume from the latest checkpoint "
+        "in the work directory.",
+    )
     parser.add_argument(
-        '--cfg-options',
-        nargs='+',
+        "--cfg-options",
+        nargs="+",
         action=DictAction,
-        help='override some settings in the used config, the key-value pair '
-        'in xxx=yyy format will be merged into config file. If the value to '
+        help="override some settings in the used config, the key-value pair "
+        "in xxx=yyy format will be merged into config file. If the value to "
         'be overwritten is a list, it should be like key="[a,b]" or key=a,b '
         'It also allows nested list/tuple values, e.g. key="[(a,b),(c,d)]" '
-        'Note that the quotation marks are necessary and that no white space '
-        'is allowed.')
+        "Note that the quotation marks are necessary and that no white space "
+        "is allowed.",
+    )
     parser.add_argument(
-        '--launcher',
-        choices=['none', 'pytorch', 'slurm', 'mpi'],
-        default='none',
-        help='job launcher')
-    parser.add_argument('--local_rank', type=int, default=0)
+        "--launcher",
+        choices=["none", "pytorch", "slurm", "mpi"],
+        default="none",
+        help="job launcher",
+    )
+    parser.add_argument("--local_rank", type=int, default=0)
     args = parser.parse_args()
-    if 'LOCAL_RANK' not in os.environ:
-        os.environ['LOCAL_RANK'] = str(args.local_rank)
+    if "LOCAL_RANK" not in os.environ:
+        os.environ["LOCAL_RANK"] = str(args.local_rank)
 
     return args
 
@@ -71,36 +75,51 @@ def main():
     if args.work_dir is not None:
         # update configs according to CLI args if args.work_dir is not None
         cfg.work_dir = args.work_dir
-    elif cfg.get('work_dir', None) is None:
+    elif cfg.get("work_dir", None) is None:
         # use config filename as default work_dir if cfg.work_dir is None
-        cfg.work_dir = osp.join('./work_dirs',
-                                osp.splitext(osp.basename(args.config))[0])
+        cfg.work_dir = osp.join(
+            "./work_dirs", osp.splitext(osp.basename(args.config))[0]
+        )
 
     # enable automatic-mixed-precision training
     if args.amp is True:
         optim_wrapper = cfg.optim_wrapper.type
-        if optim_wrapper == 'AmpOptimWrapper':
+        if optim_wrapper == "AmpOptimWrapper":
             print_log(
-                'AMP training is already enabled in your config.',
-                logger='current',
-                level=logging.WARNING)
+                "AMP training is already enabled in your config.",
+                logger="current",
+                level=logging.WARNING,
+            )
         else:
-            assert optim_wrapper == 'OptimWrapper', (
-                '`--amp` is only supported when the optimizer wrapper type is '
-                f'`OptimWrapper` but got {optim_wrapper}.')
-            cfg.optim_wrapper.type = 'AmpOptimWrapper'
-            cfg.optim_wrapper.loss_scale = 'dynamic'
+            assert optim_wrapper == "OptimWrapper", (
+                "`--amp` is only supported when the optimizer wrapper type is "
+                f"`OptimWrapper` but got {optim_wrapper}."
+            )
+            cfg.optim_wrapper.type = "AmpOptimWrapper"
+            cfg.optim_wrapper.loss_scale = "dynamic"
 
     # resume is determined in this priority: resume from > auto_resume
-    if args.resume == 'auto':
+    if args.resume == "auto":
         cfg.resume = True
         cfg.load_from = None
     elif args.resume is not None:
         cfg.resume = True
         cfg.load_from = args.resume
 
+    if wandb_vis_index := next(
+        (
+            i
+            for i, hook in enumerate(cfg.vis_backends)
+            if hook.type == "WandbVisBackend"
+        ),
+        None,
+    ):
+        wandb_vis = cfg.vis_backends[wandb_vis_index]
+        wandb_vis.init_kwargs.name = osp.basename(args.config)
+        wandb_vis.init_kwargs.tag = osp.basename(args.config).split("-")
+
     # build the runner from config
-    if 'runner_type' not in cfg:
+    if "runner_type" not in cfg:
         # build the default runner
         runner = Runner.from_cfg(cfg)
     else:
@@ -112,5 +131,5 @@ def main():
     runner.train()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
